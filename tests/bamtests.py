303f362a22cf9f5ad6c6701562b704f821b45927
Mark Final <markfinal@hotmail.com>
2015-09-18 21:34:24 +0100

    [tests] Adding configuration for tests

---
+++ tests/bamtests.py
@@ -0,0 +1,9 @@
+from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+
+def ConfigureRepository():
+    configs = {}
+    configs["Qt4Test1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
+    configs["Qt5Test1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64]},
+                                    linux={"Native":[gcc64],"MakeFile":[gcc64]},
+                                    osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
+    return configs
