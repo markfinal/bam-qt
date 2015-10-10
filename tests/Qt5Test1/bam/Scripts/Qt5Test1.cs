1c69fa1a6eaee1d6542b16d2176da7a9ea255227
Mark Final <markfinal@hotmail.com>
2015-10-10 10:03:19 +0100

    [tests,Qt5Test1] Get the name of the platform plugin from the Qt packages

--- tests/Qt5Test1/bam/Scripts/Qt5Test1.cs
+++ tests/Qt5Test1/bam/Scripts/Qt5Test1.cs
@@ -120,11 +120,7 @@ namespace Qt5Test1
                 this.IncludeFramework<Qt.GuiFramework>("../Frameworks", app);
                 this.IncludeFramework<Qt.PrintSupportFramework>("../Frameworks", app);
 
-                // TODO: get the path from the Qt package for the platform plugin
-                this.IncludeFile("/Users/mark/Qt5.3.2/5.3/clang_64/plugins/platforms/libqcocoa.dylib",
-                                 "../Plugins/qtplugins/platforms",
-                                 app,
-                                 isExecutable: true);
+                this.Include<Qt.PlatformPlugin>(C.Plugin.Key, "../Plugins/qtplugins/platforms", app);
 
                 this.IncludeFile(this.CreateTokenizedString("$(packagedir)/resources/osx/qt.conf"),
                                  "../Resources",
