d62cf2a223926700e3ef1e2c58c7000ef8cece5b
Mark Final <markfinal@hotmail.com>
2015-12-01 17:10:07 +0000

    [packages,QtCommon,*] Moc'd sources inherit the relative paths of their headers to the package directory. Allows multiple files with the same basename to be moc'd from the same package

--- packages/QtCommon-5.x/bam/Scripts/Moc/MocGeneratedSource.cs
+++ packages/QtCommon-5.x/bam/Scripts/Moc/MocGeneratedSource.cs
@@ -54,7 +54,7 @@ namespace QtCommon
             {
                 this.SourceHeaderModule = value;
                 this.DependsOn(value);
-                this.GeneratedPaths[Key].Aliased(this.CreateTokenizedString("$(encapsulatingbuilddir)/$(config)/@basename($(0))_moc.cpp", value.GeneratedPaths[C.HeaderFile.Key]));
+                this.GeneratedPaths[Key].Aliased(this.CreateTokenizedString("$(encapsulatingbuilddir)/$(config)/@changeextension(@trimstart(@relativeto($(0),$(packagedir)),../),.moc.cpp)", value.GeneratedPaths[C.HeaderFile.Key]));
                 this.GetEncapsulatingReferencedModule(); // or the path above won't be parsable prior to all modules having been created
             }
         }
