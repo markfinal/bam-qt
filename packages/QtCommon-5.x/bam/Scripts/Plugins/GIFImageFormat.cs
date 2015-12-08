eeae399208f8a9401bd2fdc94f00780df781a6bc
Mark Final <markfinal@hotmail.com>
2015-12-08 17:38:01 +0000

    [packages,Qt*,5*] Added GIF image format plugin

--- packages/QtCommon-5.x/bam/Scripts/Plugins/GIFImageFormat.cs
+++ packages/QtCommon-5.x/bam/Scripts/Plugins/GIFImageFormat.cs
@@ -27,17 +27,19 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
-namespace Qt
+using Bam.Core;
+namespace QtCommon
 {
-    public sealed class PlatformPlugin :
-        QtCommon.PlatformPlugin
-    { }
-
-    public sealed class XCBGLIntegrationsPlugin :
-        QtCommon.XCBGLIntegrations
-    { }
-
-    public sealed class GIFImageFormatPlugin :
-        QtCommon.GIFImageFormatPlugin
-    { }
+    public abstract class GIFImageFormatPlugin :
+        CommonPlugin
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+            this.Macros.AddVerbatim("QtPluginDir", "imageformats");
+            this.Macros.AddVerbatim("QtPluginName", "qgif");
+        }
+    }
 }
