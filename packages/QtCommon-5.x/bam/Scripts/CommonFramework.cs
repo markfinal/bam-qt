a374adb4d460a02bbf786c5b72796601930e6a01
Mark Final <markfinal@hotmail.com>
2018-06-22 15:58:16 +0100

    Updated for latest BAM 120

--- packages/QtCommon-5.x/bam/Scripts/CommonFramework.cs
+++ packages/QtCommon-5.x/bam/Scripts/CommonFramework.cs
@@ -90,8 +90,8 @@ namespace QtCommon
             }
         }
 
-        public override void
-        Evaluate()
+        protected override void
+        EvaluateInternal()
         {
             this.ReasonToExecute = null;
         }
