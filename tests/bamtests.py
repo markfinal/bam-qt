2d0f1a9dd7d5f507ae3daa4b4ae2916ce14d6361
Mark Final <markfinal@hotmail.com>
2019-06-14 19:08:45 +0100

    [CI] First stab at CI, just downloading Qt on all platforms. Running a test will fail currently, as there's no install step for Qt

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -1,15 +1,21 @@
-from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+from testconfigurations import TestSetup, visualc, visualc64, visualc32, mingw32, gcc, gcc64, gcc32, clang, clang32, clang64
 
 def configure_repository():
     configs = {}
-    configs["Qt4Test1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
-    configs["Qt5Test1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64]},
-                                    linux={"Native":[gcc64],"MakeFile":[gcc64]},
-                                    osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
-    configs["Qt5Test2"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64]},
-                                    linux={"Native":[gcc64],"MakeFile":[gcc64]},
-                                    osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
-    configs["Qt5WebBrowsingTest"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64]},
-                                              linux={"Native":[gcc64],"MakeFile":[gcc64]},
-                                              osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
+    #configs["Qt4Test1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
+    configs["Qt5Test1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                    linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                    osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["Qt5Test2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                    linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                    osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["Qt5Test3"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                    linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                    osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["Qt5Test4"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                    linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                    osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["Qt5WebBrowsingTest"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                             linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                             osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     return configs
